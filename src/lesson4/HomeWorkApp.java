0c8200007ba08cdb3e64b409b9c88d2c2d3c7806
DMelnev <59581747+DMelnev@users.noreply.github.com>
2021-12-10 00:34:29 +0300

    initMap, printMap

--- src/lesson4/HomeWorkApp.java
+++ src/lesson4/HomeWorkApp.java
@@ -1,7 +1,30 @@
 package lesson4;
 
-public class HomeWorkApp {
-    public static void main(String[] args){
+import java.util.Arrays;
 
+public class HomeWorkApp {
+    public static char[][] map;
+    public static final int SIZE = 5;
+    public static final int SET = 3;
+
+    public static char DOT_EMPTY = '•';
+    public static char DOT_HUMAN = 'X';
+    public static char DOT_AI = 'O';
+
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (char[] line : map) Arrays.fill(line, DOT_EMPTY);
+    }
+
+    public static void printMap() {
+        for (char[] line : map) {
+            for (char point : line) System.out.print(point + "  ");
+            System.out.println();
+        }
     }
 }
