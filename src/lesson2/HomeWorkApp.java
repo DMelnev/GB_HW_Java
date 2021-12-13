6f88e3903ec45ff5190d7570e07ce4dac3741d5a
Dmitry <dvmau@mail.ru>
2021-12-13 22:30:32 +0300

    end

--- src/lesson2/HomeWorkApp.java
+++ src/lesson2/HomeWorkApp.java
@@ -3,31 +3,31 @@ package lesson2;
 public class HomeWorkApp {
     public static void main(String[] args) {
 
-        //checking result of exercise one
-        int someNumber1 = 15, someNumber2 = 6;
-        System.out.printf("%s + %s = %s. Result is %s 10 and 20\n",
-                someNumber1, someNumber2, (someNumber1 + someNumber2),
-                (checkBetween10_20(someNumber1, someNumber2)) ? "between" : "beyond");
-
-        //checking result of exercise two
-        someNumber1 = 5;
-        checkPositive(someNumber1);
-
-        //checking result of exercise three
-        someNumber1 = -5;
-        System.out.printf("Number %s is %s.\n", someNumber1, (checkNegative(someNumber1)) ? "negative" : "positive");
-
-        //checking result of exercise four
-        someNumber1 = 3;
-        String str = "Some String";
-        writeString(str, someNumber1);
-
-        //checking result of exercise five
-        int year = 2400;
-        System.out.printf("%s%s leap year.\n", year, (isYearLeap(year)) ? "" : " isn't a");
+//        //checking result of exercise one
+//        int someNumber1 = 15, someNumber2 = 6;
+//        System.out.printf("%s is %s 10 and 20\n", (someNumber1 + someNumber2),
+//                (checkBetween10_20(someNumber1, someNumber2)) ? "between" : "beyond");
+//
+//        //checking result of exercise two
+//        someNumber1 = 5;
+//        checkPositive(someNumber1);
+//
+//        //checking result of exercise three
+//        someNumber1 = -5;
+//        System.out.printf("Number %s is %s.\n", someNumber1, (checkNegative(someNumber1)) ? "negative" : "positive");
+//
+//        //checking result of exercise four
+//        someNumber1 = 3;
+//        String str = "Some String";
+//        writeString(str, someNumber1);
+//
+//        //checking result of exercise five
+//        int year = 2400;
+//        System.out.printf("%s%s leap year.\n", year, (isYearLeap(year)) ? "" : " isn't a");
     }
 
     public static boolean checkBetween10_20(int a, int b) {          // first exercise
+
         return ((a + b) >= 10) && ((a + b) <= 20);
     }
 
@@ -37,6 +37,7 @@ public class HomeWorkApp {
     }
 
     public static boolean checkNegative(int a) {                      // third exercise
+
         return a < 0;
     }
 
@@ -46,16 +47,8 @@ public class HomeWorkApp {
         }
     }
 
-    public static void writeString(int b, String str) {                //overload for variant
-        int i = 0;
-        while (i < b) {
-            System.out.println(str);
-            i++;
-        }
-
-    }
-
     public static boolean isYearLeap(int year) {                       // fifth exercise
+
         return (year % 4 == 0) && ((year % 100 > 0) || (year % 400 == 0));
     }
 
