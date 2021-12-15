dfe26cce3c6b99fcb31cc008d8c40e645044e3c5
DMITRIY <dvmau@mail.ru>
2021-12-15 04:45:07 +0300

    final

--- src/lesson3/HomeWorkApp.java
+++ src/lesson3/HomeWorkApp.java
@@ -1,9 +1,8 @@
 /**
- * Java 1. HomeWork 3
+ * Java 1. Homework 3
  *
  * @author Melnev Dmitry
- * @version created 05.12.2021 corrected 15.12.2021
- *
+ * @version created at 06.12.2021 corrected at 15.12.2021
  */
 package lesson3;
 
@@ -11,87 +10,119 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class HomeWorkApp {
+    static Random rnd = new Random();
+
     public static void main(String[] args) {
 
-        //exercise one_
-        //prepare:
-        Random rnd = new Random();
-        int[] arrayInt = new int[10];
-        for (int i = 0; i < arrayInt.length; i++) {
-            arrayInt[i] = rnd.nextInt(2);
-        }
         System.out.println("exercise 1:");
+        int[] arrayInt = createAndFillRandomArray(12, 2); // part one
         System.out.println(Arrays.toString(arrayInt));
-        // solution:
-        for (int i = 0; i < arrayInt.length; i++) arrayInt[i] ^= 1;
-        System.out.println(Arrays.toString(arrayInt));
+        System.out.println(Arrays.toString(invertArray(arrayInt))); //part two
 
-        //exercise two
-        int[] arrayInt_2 = new int[100];
-        for (int i = 0; i < arrayInt_2.length; i++) arrayInt_2[i] = i + 1;
-        System.out.println("exercise 2:");
-        System.out.println(Arrays.toString(arrayInt_2));
+        System.out.println("\nexercise 2:");
+        System.out.println(Arrays.toString(createLinearArray(100)));
 
-        //exercise three
+        System.out.println("\nexercise 3:");
         int[] arrayInt_3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arrayInt_3.length; i++) {
-            arrayInt_3[i] *= (arrayInt_3[i] < 6) ? 2 : 1;
-        }
-        System.out.println("exercise 3:");
         System.out.println(Arrays.toString(arrayInt_3));
+        System.out.println(Arrays.toString(modifyArray(arrayInt_3)));
 
-        //exercise four:
-        int[][] arrayInt_4 = new int[10][10];
-        for (int i = 0; i < arrayInt_4.length; i++) {
-            for (int j = 0; j < arrayInt_4[0].length; j++) {
-                arrayInt_4[i][j] = (i == j || i == arrayInt_4[0].length - j - 1) ? 1 : 0;
-            }
-        }
-        System.out.println("exercise 4:");
-        for (int[] ints : arrayInt_4) System.out.println(Arrays.toString(ints));
+        System.out.println("\nexercise 4:");
+        for (int[] line : createCrossArray(7))
+            System.out.println(Arrays.toString(line));
 
         //exercise five:
-        System.out.println("exercise 5:");
+        System.out.println("\nexercise 5:");
         System.out.println(Arrays.toString(createAndFillArray(7, 71)));
 
-        //exercise six
+        System.out.println("\nexercise 6:");
         int[] arrayInt_6 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        int maxValue = arrayInt_6[0], minValue = arrayInt_6[0];
-        for (int j : arrayInt_6) {
-            maxValue = Math.max(j, maxValue);
-            minValue = Math.min(j, minValue);
+        System.out.println(Arrays.toString(arrayInt_6));
+        System.out.printf("min value is %d, max value is %d\n", getMinElement(arrayInt_6), getMaxElement(arrayInt_6));
+
+        System.out.println("\nexercise 7:");
+        int[][] arrayInt_7 = {{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}, //false
+                {1, 5, 3, 2, 11, 4, 5, 2, 4, 7},//true
+                {1, 5, 6},//true
+                {12, 1, 4, 56, 123, 23, -4, 12, 65, 76, 34, 13, 23}}; // true
+        for (int[] tempArray : arrayInt_7) {
+            System.out.println(Arrays.toString(tempArray));
+            System.out.printf("Left part has %ssame right part\n\n", (findSameParts(tempArray)) ? "" : "not ");
         }
-        System.out.println("exercise 6:");
-        System.out.printf("min value is %d, max value is %d\n", minValue, maxValue);
 
-        //exercise seven:
-        //int[] arrayInt_7 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}; // false
-        int[] arrayInt_7 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 7}; // true
-        //int[] arrayInt_7 = {1, 5, 6}; // true
-        System.out.println("exercise 7:");
-        System.out.println(Arrays.toString(arrayInt_7));
-//        System.out.printf("Left part has %ssame right part\n", (findSameParts(arrayInt_7)) ? "" : "not ");
-        System.out.print("Left part has ");
-        if (!findSameParts(arrayInt_7)) System.out.print("not ");
-        System.out.println("same right part");
-
-        //exercise eight
-        int[] arrayInt_8 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 7};
-        int shifting = 12; // - left, + right
         System.out.println("exercise 8:");
-        System.out.println(Arrays.toString(arrayInt_8));
-        System.out.println(Arrays.toString(arrayShift(arrayInt_8, shifting)));
+        int[] arrayInt_8 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] shifts = {0, 1, 3, 13, 10, -1, -3, -5, -15};// - left, + right
+
+        for (int i : shifts) {
+            System.out.print("shift = " + i + " - ");
+            System.out.println(Arrays.toString(arrayShift(arrayInt_8.clone(), i)));
+            //передаю в метод копию массива, а не указатель, что бы не изменить оригинал
+        }
     }
 
+    static int[] createAndFillRandomArray(int len, int bound) {          //first exercise part one
+        int[] arrayInt = new int[len];
+        for (int i = 0; i < arrayInt.length; i++)
+            arrayInt[i] = rnd.nextInt(bound);
+        return arrayInt;
+    }
 
-    public static int[] createAndFillArray(int len, int initialValue) {         //fifth exercise
-        len = Math.max(len, 0);
+    static int[] invertArray(int[] array) {                             // first exercise part two
+        for (int i = 0; i < array.length; i++)
+            array[i] ^= 1;
+        return array;
+    }
+
+    static int[] createLinearArray(int len) {                           // second exercise
         int[] array = new int[len];
+        for (int i = 0; i < array.length; i++)
+            array[i] = i + 1;
+        return array;
+    }
+
+    static int[] modifyArray(int[] array) {                              // third exercise
+        for (int i = 0; i < array.length; i++)
+            array[i] *= (array[i] < 6) ? 2 : 1;
+        return array;
+    }
+
+    static int[][] createCrossArray(int size) {                         //fourth exercise
+        int[][] array = new int[size][size];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = (i == j || i == array[0].length - j - 1) ? 1 : 0;
+            }
+        }
+        return array;
+    }
+
+    static int[] createAndFillArray(int len, int initialValue) {         //fifth exercise
+        int[] array = new int[len];
+//        for (int i = 0; i < len; i++) array[i] = initialValue; // то же самое, что и ниже
         Arrays.fill(array, initialValue);
         return array;
     }
 
-    public static boolean findSameParts(int[] array) {                               //seventh exercise
+    static int getMaxElement(int[] array) {                             //sixth exercise part one
+        int maxValue = array[0];
+        for (int temp : array) {
+//            maxValue = (temp > maxValue) ? temp : maxValue;
+            maxValue = Math.max(temp, maxValue);
+        }
+        return maxValue;
+    }
+
+    static int getMinElement(int[] array) {                             //*sixth exercise part two
+        int minValue = array[0];
+        for (int temp : array) {
+//            minValue = (temp < minValue) ? temp : minValue;
+            minValue = Math.min(temp, minValue);
+        }
+        return minValue;
+    }
+
+    static boolean findSameParts(int[] array) {                          //**seventh exercise
         for (int i = 1; i < array.length; i++) {
             int sumLeft = 0, sumRight = 0;
             for (int j = 0; j < i; j++) sumLeft += array[j];
@@ -101,12 +132,13 @@ public class HomeWorkApp {
         return false;
     }
 
-    public static int[] arrayShift(int[] array, int shift) {                    // eighth exercise
+    static int[] arrayShift(int[] array, int shift) {                    // ***eighth exercise
         shift %= array.length;                                                          // убираем все лишнее
-        shift += (shift < 0) ? array.length : 0;                                        // выбираем направление
+        shift += (shift < 0) ? array.length : 0;                                        // корректируем отрицательное направление
         while (shift > 0) {                                                             // запускаем shift сдвигов
             int lastElement = array[array.length - 1];                                  // сохраняем последний элемент
-            System.arraycopy(array, 0, array, 1, array.length - 1); // сдвигаем на 1
+            for (int i = array.length - 2; i >= 0; i--) array[i + 1] = array[i];        // сдвигаем массив на 1 вправо
+//            System.arraycopy(array, 0, array, 1, array.length - 1);                   // то же самое что выше
             array[0] = lastElement;                                                    // восстанавливаем первый элемент
             shift--;
         }
