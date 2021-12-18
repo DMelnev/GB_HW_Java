f3ad2105c52df6c32e456d5210d65e9b5bf76145
DMelnev <59581747+DMelnev@users.noreply.github.com>
2021-12-18 20:55:43 +0300

    .

--- src/lesson5/Employee.java
+++ src/lesson5/Employee.java
@@ -8,5 +8,25 @@
  */
 package lesson5;
 
+import java.util.Calendar;
+
 public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+    private int yearOfBirth;
+    Employee(String fullName, String position, String email, String phone, double salary, int age){
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+
+        Calendar date = Calendar.getInstance();
+        yearOfBirth = date.get(Calendar.YEAR);
+    }
 }
