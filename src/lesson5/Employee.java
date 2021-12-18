38dc2edd955a6dc46bf0fb0459fe4620a47bfbef
DMelnev <59581747+DMelnev@users.noreply.github.com>
2021-12-18 18:55:15 +0300

    created classes

---
+++ src/lesson5/Employee.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * Class determinate for object employee
+ *
+ * @author Melnev Dmitry
+ * @version 2021-12-
+ *
+ */
+package lesson5;
+
+public class Employee {
+}
---
+++ src/lesson5/HomeWorkApp.java
@@ -0,0 +1,19 @@
+/**
+ *
+ * Java 1/ Lesson 5
+ *
+ * @author Melnev Dmitry
+ * @version 2021-12-
+ *
+ */
+package lesson5;
+
+public class HomeWorkApp {
+    public static void main(String[] args){
+        HomeWorkApp process = new HomeWorkApp();
+        process.run();
+    }
+    void run(){
+
+    }
+}
