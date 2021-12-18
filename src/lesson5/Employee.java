720b7c9a555fd25407be91425b5364a668a3e1d0
DMelnev <59581747+DMelnev@users.noreply.github.com>
2021-12-18 22:38:20 +0300

    .

--- src/lesson5/Employee.java
+++ src/lesson5/Employee.java
@@ -1,10 +1,8 @@
 /**
- *
  * Class determinate for object employee
  *
  * @author Melnev Dmitry
  * @version 2021-12-
- *
  */
 package lesson5;
 
@@ -18,7 +16,10 @@ public class Employee {
     private double salary;
     private int age;
     private int yearOfBirth;
-    Employee(String fullName, String position, String email, String phone, double salary, int age){
+    static int currentYear = 0;
+
+
+    Employee(String fullName, String position, String email, String phone, double salary, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
@@ -26,7 +27,36 @@ public class Employee {
         this.salary = salary;
         this.age = age;
 
-        Calendar date = Calendar.getInstance();
-        yearOfBirth = date.get(Calendar.YEAR);
+        yearOfBirth = (currentYear > 0) ? currentYear - this.age : 0;
+    }
+
+    void printInfo() {
+        System.out.printf("Employer name: %s,\nyear of birth: %d, age: %d\n" +
+                        "phone number: %s,\nemail: %s\n" +
+                        "position: %s,\nsalary: %,.2f\n",
+                fullName, yearOfBirth, age,
+                phone, email,
+                position, salary);
+    }
+
+    static void setCurrentYear(int year) {
+        currentYear = year;
+    }
+    void setFullName(String name){
+        fullName = name;
+    }
+    void setAge(int age){
+        this.age = age;
+        yearOfBirth = (currentYear > 0) ? currentYear - this.age : 0;
+    }
+    void setPosition(String position){
+        this.position = position;
+    }
+    void setEmail(String email){
+
+    }
+
+    int getYearOfBirth() {
+        return yearOfBirth;
     }
 }
--- src/lesson5/HomeWorkApp.java
+++ src/lesson5/HomeWorkApp.java
@@ -1,19 +1,41 @@
 /**
- *
  * Java 1/ Lesson 5
  *
  * @author Melnev Dmitry
  * @version 2021-12-
- *
  */
 package lesson5;
 
+import java.util.Calendar;
+
 public class HomeWorkApp {
-    public static void main(String[] args){
+    Calendar date = Calendar.getInstance();
+
+    public static void main(String[] args) {
         HomeWorkApp process = new HomeWorkApp();
         process.run();
     }
-    void run(){
+
+    void run() {
+
+        Employee.setCurrentYear(date.get(Calendar.YEAR));
+
+        Employee[] personArr = new Employee[5];
+        personArr[0] = new Employee("Ivan Ivanov",
+                "director", "asd@asd.ru", "+7900 900 00 00", 150000.0, 48);
+        personArr[1] = new Employee("Petr Petrov",
+                "manager", "qwe@asd.ru", "+7900 900 00 01", 130000.0, 32);
+        personArr[2] = new Employee("Sidor Sidorov",
+                "lawer", "zxc@asd.ru", "+7900 900 00 02", 120000.0, 41);
+        personArr[3] = new Employee("Dmitriy Dmitryev",
+                "accountant", "fgh@asd.ru", "+7900 900 00 03", 100000.0, 52);
+        personArr[4] = new Employee("Alexandr Alexandrov",
+                "engineer", "rty@asd.ru", "+7900 900 00 04", 25000.0, 35);
+
+        for (Employee prsn : personArr){
+            prsn.printInfo();
+            System.out.println();
+        }
 
     }
 }
