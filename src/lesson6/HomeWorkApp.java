c5c0d3a22daa3459ae5ae65854aa5218e5596222
DMITRIY <dvmau@mail.ru>
2021-12-24 16:13:41 +0300

    create

--- src/lesson6/HomeWorkApp.java
+++ src/lesson6/HomeWorkApp.java
@@ -1,4 +1,26 @@
 package lesson6;
 
 public class HomeWorkApp {
+    static void main(String[] args){
+        System.out.println(TestParent.test(1));
+
+    }
+
+}
+
+class TestClass extends TestParent {
+
+    static int test(int asd) {
+        asd ^= 1;
+        return asd;
+    }
+}
+
+class TestParent {
+    static int asd;
+
+    static int test(int asd) {
+        asd += asd;
+        return asd;
+    }
 }
