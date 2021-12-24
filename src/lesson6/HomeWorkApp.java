297c3b4ba6414b36e07f1915fe4ca45c25cabff9
DMITRIY <dvmau@mail.ru>
2021-12-24 16:45:43 +0300

    test override

--- src/lesson6/HomeWorkApp.java
+++ src/lesson6/HomeWorkApp.java
@@ -1,8 +1,7 @@
-package lesson6;
-
 public class HomeWorkApp {
-    static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(TestParent.test(1));
+        System.out.println(TestClass.test(1));
 
     }
 
